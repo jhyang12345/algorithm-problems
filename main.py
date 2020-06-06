c31fce16028b26318179700bdabb7b010750381c
jhyang12345 <jhyang12345@naver.com>
2020-06-06 23:11:31 +0900

    Got get_problem method to work

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import sys
 import os
 
+problem_directory = 'problems'
+input_directory = 'inputs'
+
+def get_problem(title):
+    problems = os.listdir(problem_directory)
+    p_name = title + ".py"
+    p_name = p_name.lower()
+    for problem in problems:
+        if problem.lower() == p_name:
+            return os.path.join(problem_directory, problem)
+    print("File not found")
+    return None
+
+
 if __name__ == '__main__':
-    pass
+    args = sys.argv[1:]
+    title = args[-1]
+    problem = get_problem(title)
+    print(problem)
