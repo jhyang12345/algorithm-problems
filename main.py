bcb34ac7bd86514716f8a8f1fe801ad9d62df7a0
jhyang12345 <jhyang12345@naver.com>
2020-06-06 23:44:32 +0900

    Added feed_inputs_to_file method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import subprocess
 
 problem_directory = 'problems'
 input_directory = 'inputs'
@@ -24,8 +25,25 @@ def get_inputs(title):
             files.append(os.path.join(input_directory, i))
     return files
 
+def feed_inputs_to_file(problem, inputs):
+    if len(inputs) == 0:
+        os.system(f'python {problem}')
+    else:
+        for i in inputs:
+            process = subprocess.Popen(['python', problem],
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         encoding='utf8')
+            with open(i, 'r') as file:
+                input_data = file.read()
+
+            stdoutdata, stderrdata = process.communicate(input = input_data)
+            print(stdoutdata)
+
 if __name__ == '__main__':
     args = sys.argv[1:]
     title = args[-1]
     problem = get_problem(title)
     inputs = get_inputs(title)
+    feed_inputs_to_file(problem, inputs)
