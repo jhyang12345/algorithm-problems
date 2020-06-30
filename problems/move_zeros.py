79a098a4874f41c116625db564e15986645f4a39
jhyang12345 <jhyang12345@naver.com>
2020-07-01 00:02:11 +0900

    Solved move zeros better than before

--- problems/move_zeros.py
+++ problems/move_zeros.py
@@ -9,3 +9,20 @@ def move_zeros(arr):
 nums = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
 print(nums)
 print(move_zeros(nums))
+
+
+def move_zeros_2(arr):
+    last_num = 0
+    for i in range(len(arr)):
+        cur = arr[i]
+        if cur != 0:
+            arr[last_num] = cur
+            arr[i] = 0
+            last_num += 1
+    return arr
+
+
+
+nums = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
+print(nums)
+print(move_zeros_2(nums))
