ca8a0bde6c392679e501a058e7b5f34ace42920c
jhyang12345 <jhyang12345@naver.com>
2020-07-01 00:06:45 +0900

    Update zeros added

--- problems/move_zeros.py
+++ problems/move_zeros.py
@@ -16,13 +16,18 @@ def move_zeros_2(arr):
     for i in range(len(arr)):
         cur = arr[i]
         if cur != 0:
-            arr[last_num] = cur
-            arr[i] = 0
+            if last_num != i:
+                arr[last_num] = cur
+                arr[i] = 0
             last_num += 1
     return arr
 
 
 
 nums = [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
+print(nums)
+print(move_zeros_2(nums))
+
+nums = [1, 2, 3, 4, 0]
 print(nums)
 print(move_zeros_2(nums))
