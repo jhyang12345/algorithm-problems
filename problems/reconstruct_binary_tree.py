9c7c30842fb8f985c295228e5b6a0067205cbbe6
jhyang12345 <jhyang12345@naver.com>
2020-07-20 21:09:31 +0900

    Added comments...

--- problems/reconstruct_binary_tree.py
+++ problems/reconstruct_binary_tree.py
@@ -1,5 +1,18 @@
 from collections import deque
 
+# You are given the preorder and inorder traversals of a binary tree in the form of arrays.
+# Write a function that reconstructs the tree represented by these traversals.
+#
+# Example:
+# Preorder: [a, b, d, e, c, f, g]
+# Inorder: [d, b, e, a, f, c, g]
+#
+#     a
+#    / \
+#   b   c
+#  / \ / \
+# d  e f  g
+
 class Node(object):
     def __init__(self, val):
         self.val = val
