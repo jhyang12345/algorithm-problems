a04870c2eda534000ba2e8dd1afc4683446feddd
jhyang12345 <jhyang12345@naver.com>
2020-07-23 23:14:03 +0900

    Added a geeksforgeeks solution for reference

--- problems/three_sum.py
+++ problems/three_sum.py
@@ -28,3 +28,41 @@ print(three_sum(nums))
 
 nums = [1, -2, 1, 0, 5]
 print(three_sum(nums))
+
+
+# https://www.geeksforgeeks.org/find-triplets-array-whose-sum-equal-zero/
+# def findTriplets(arr, n):
+#
+#     found = False
+#
+#     # sort array elements
+#     arr.sort()
+#
+#     for i in range(0, n-1):
+#
+#         # initialize left and right
+#         l = i + 1
+#         r = n - 1
+#         x = arr[i]
+#         while (l < r):
+#
+#             if (x + arr[l] + arr[r] == 0):
+#                 # print elements if it's sum is zero
+#                 print(x, arr[l], arr[r])
+#                 l+=1
+#                 r-=1
+#                 found = True
+#
+#
+#             # If sum of three elements is less
+#             # than zero then increment in left
+#             elif (x + arr[l] + arr[r] < 0):
+#                 l+=1
+#
+#             # if sum is greater than zero than
+#             # decrement in right side
+#             else:
+#                 r-=1
+#
+#     if (found == False):
+#         print(" No Triplet Found")
