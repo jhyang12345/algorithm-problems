bf5f62046feb175607b9a047a542a4b95fc82d99
jhyang12345 <jhyang12345@naver.com>
2020-08-08 23:24:49 +0900

    Added comments

--- problems/min_ranges_need_to_sort.py
+++ problems/min_ranges_need_to_sort.py
@@ -1,3 +1,7 @@
+# Given a list of integers, return the bounds of the minimum range that must be sorted so that the whole list would be sorted.
+#
+# https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/
+
 def findRange(nums):
     s = sorted(nums)
     start = len(s)
