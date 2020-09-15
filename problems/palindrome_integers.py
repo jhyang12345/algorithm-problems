73447639ca6a080e42109ff661c5cefdc03dae63
jhyang12345 <jhyang12345@naver.com>
2020-09-15 23:43:54 +0900

    Solving is palindrome

---
+++ problems/palindrome_integers.py
@@ -0,0 +1,12 @@
+# Given an integer, check if that integer is a palindrome. For this problem do not 
+# convert the integer to a string to check if it is a palindrome.
+
+import math
+
+def is_palindrome(n):
+
+
+print(is_palindrome(1234321))
+# True
+print(is_palindrome(1234322))
+# False
