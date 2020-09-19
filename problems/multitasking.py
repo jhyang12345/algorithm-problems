9070ee92db74faa0a95fd8416898b2bcbd1d13e9
jhyang12345 <jhyang12345@naver.com>
2020-09-19 23:58:09 +0900

    Believed to have solved multitasking...

--- problems/multitasking.py
+++ problems/multitasking.py
@@ -5,7 +5,19 @@
 # cooldown = 2
 # output: 7 (order is 1 _ _ 1 2 _ 1)
 def findTime(arr, cooldown):
-  # Fill this in.
+    tq = {}
+    cap = 0
+    for num in arr:
+        if num not in tq:
+            tq[num] = cap + num + cooldown
+        else:
+            cap = tq[num]
+            tq[num] = cap + num + cooldown
+    max_val = 0
+    for num in tq:
+        max_val = max(max_val, tq[num])
+    return max_val - cooldown
 
-print findTime([1, 1, 2, 1], 2)
+
+print(findTime([1, 1, 2, 1], 2))
 # 7
