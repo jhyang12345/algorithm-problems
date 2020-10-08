8403d62eab3a9a43a6ed2bfe69e294368a01fe24
jhyang12345 <jhyang12345@naver.com>
2020-10-08 23:57:12 +0900

    Solving reverse polish notation...

---
+++ problems/reverse_polish_notation_calculator.py
@@ -0,0 +1,19 @@
+# Given an expression (as a list) in reverse polish notation,
+# evaluate the expression. Reverse polish notation is where
+# the numbers come before the operand. Note that there can be
+# the 4 operands '+', '-', '*', '/'. You can also assume the expression will be well formed.
+#
+# Example
+# Input: [1, 2, 3, '+', 2, '*', '-']
+# Output: -9
+# The equivalent expression of the above reverse polish notation would be (1 - ((2 + 3) * 2)).
+#
+# Here's some starter code:
+
+def reverse_polish_notation(expr):
+    ret = 0
+    return ret
+
+# 1 - (2 + 3) * 2
+print(reverse_polish_notation([1, 2, 3, '+', 2, '*', '-']))
+# -9
