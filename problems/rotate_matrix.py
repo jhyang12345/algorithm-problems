889a099b4ac18cf424c53dc43317295eb254aa46
jhyang12345 <jhyang12345@naver.com>
2020-11-04 02:33:22 +0900

    Solved rotate matrix finally...

--- problems/rotate_matrix.py
+++ problems/rotate_matrix.py
@@ -2,12 +2,50 @@
 #
 # Here's an example and some starting code:
 
+import math
+
+dirs = [(0, 1), (1, 0), (0, -1) ,(-1, 0)]
+
+def get_diff(x, y, mid):
+    return max(abs(x * 10 - mid), abs(y * 10 - mid))
+
+def get_dest_coord(mat, x, y):
+    track = [(x, y)]
+    n = len(mat)
+    mid = (n - 1) * 5
+    i = 0
+    while True:
+        x, y = track[-1]
+        cur_diff = get_diff(x, y, mid)
+        a, b = dirs[i]
+        next = (x + a, y + b)
+        next_x, next_y = next
+        diff = get_diff(next_x, next_y, mid)
+        if next == track[0]:
+            break
+        if diff != cur_diff:
+            i += 1
+            if i == 4:
+                break
+            continue
+        else:
+            track.append(next)
+    return track
+
 def rotate(mat):
     n = len(mat)
-    mid = math.ceil(n / 2)
-    for x in range(n):
-        for y in range(n):
-            
+    for x in range(math.ceil(n / 2)):
+        track = get_dest_coord(mat, x, x)
+        moves = n - x - 1
+        values = []
+        for t in track:
+            values.append(mat[t[0]][t[1]])
+        new_values = values[- moves:] + values[: -moves]
+        for i, t in enumerate(track):
+            mat[t[0]][t[1]] = new_values[i]
+    return mat
+
+
 
 mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # Looks like
@@ -21,3 +59,7 @@ mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # 9 6 3
 print(rotate(mat))
 # [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+mat = [[1, 2, 3, 4], [4, 5, 6, 7], [7, 8, 9, 10], [10, 11, 12, 13]]
+
+print(rotate(mat))
