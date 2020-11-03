16966e619ac8a035e5a3a4880c7b07a5bed4ea32
jhyang12345 <jhyang12345@naver.com>
2020-11-03 23:30:24 +0900

    Attempting to solve rotate matrix... for now

---
+++ problems/rotate_matrix.py
@@ -0,0 +1,23 @@
+# Given a square 2D matrix (n x n), rotate the matrix by 90 degrees clockwise.
+#
+# Here's an example and some starting code:
+
+def rotate(mat):
+    n = len(mat)
+    mid = math.ceil(n / 2)
+    for x in range(n):
+        for y in range(n):
+            
+
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Looks like
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+# Looks like
+# 7 4 1
+# 8 5 2
+# 9 6 3
+print(rotate(mat))
+# [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
