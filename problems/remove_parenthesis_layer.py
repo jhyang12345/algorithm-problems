b6d2c380212769b3db2da1ff252cd603c238c1ae
jhyang12345 <jhyang12345@naver.com>
2020-11-12 23:47:54 +0900

    Solved remove parenthesis layer problem

---
+++ problems/remove_parenthesis_layer.py
@@ -0,0 +1,44 @@
+# Given a valid parenthesis string (with only '(' and ')',
+# an open parenthesis will always end with a close parenthesis,
+# and a close parenthesis will never start first), remove the
+# outermost layers of the parenthesis string and return the new parenthesis string.
+#
+# If the string has multiple outer layer parenthesis (ie (())()),
+# remove all outer layers and construct the new string.
+# So in the example, the string can be broken down into (()) + ().
+# By removing both components outer layer we are left with () + ''
+# which is simply (), thus the answer for that input would be ().
+#
+# Here are some examples and some starter code.
+
+def remove_outermost_parenthesis(s):
+    stack = []
+    openings = []
+    for i, c in enumerate(s):
+        if c == "(":
+            if not stack:
+                openings.append(i)
+            stack.append(c)
+        elif c == ")":
+            stack.pop()
+            if not stack:
+                openings.append(i)
+    index = 0
+    ret = ''
+    for i, c in enumerate(s):
+        if i == openings[index] and index < len(openings):
+            index += 1
+            continue
+        else:
+            ret += c
+    return ret
+
+
+print(remove_outermost_parenthesis('(())()'))
+# ()
+
+print(remove_outermost_parenthesis('(()())'))
+# ()()
+
+print(remove_outermost_parenthesis('()()()'))
+# ' '
