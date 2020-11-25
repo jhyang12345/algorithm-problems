a70670bb42c54556192c0cf83ebfe469b0162392
jhyang12345 <jhyang12345@naver.com>
2020-11-25 22:28:57 +0900

    Solving convert fraction to decimal...

---
+++ problems/convert_fraction_to_decimal.py
@@ -0,0 +1,44 @@
+# Given a numerator and a denominator, find what the equivalent decimal representation is as a string.
+# If the decimal representation has recurring digits, then put those digits in brackets
+# (ie 4/3 should be represented by 1.(3) to represent 1.333...). Do not use any built in
+# evaluator functions like python's eval. You can also assume that the denominator will be nonzero.
+#
+# Here's some examples and some starter code:
+
+def frac_to_dec(numerator, denominator):
+    ret = ''
+    number = numerator
+    if denominator * numerator < 0:
+        ret += '-'
+        number = - numerator
+    numerator = abs(numerator)
+    denominator = abs(denominator)
+    value = 1
+    last = value
+    i = 0
+    while True:
+        if number * value / last > denominator:
+            if value == 10:
+                ret += '.'
+            ret += str((number * value / last) // denominator)
+            number = number % denominator
+            last = value
+        else:
+            ret += '0'
+        value *= 10
+        print(value, ret)
+
+        i += 1
+        if i > 10:
+            break
+    return ret
+
+
+print(frac_to_dec(-3, 2))
+# -1.5
+
+print(frac_to_dec(4, 3))
+# 1.(3)
+
+print(frac_to_dec(1, 6))
+# 0.1(6)
