59435d5acc8b1fc0c33d1cb4bc281aec82a5b3de
jhyang12345 <jhyang12345@naver.com>
2021-03-01 22:41:17 +0900

    Solved max number of overlaps O(nlogn)

---
+++ google_prep/max_number_overlaps.py
@@ -0,0 +1,28 @@
+def max_overlapping_intervals(pairs):
+    steps = []
+    for pair in pairs:
+        steps.append([pair[0], 1])
+        steps.append([pair[1], -1])
+    steps.sort()
+    i = 0
+    count = 0
+    ret = 0
+    index = 0
+    while i < len(steps):
+        val = steps[i][0]
+        count += steps[i][1]
+        i += 1
+        ret = max(count, ret)
+        if ret == count:
+            index = val
+    ret = max(count, ret)
+    if ret == count:
+        index = val
+    return ret
+
+
+
+
+
+pairs = [[1, 8], [2, 5], [5, 6], [3, 7]]
+print(max_overlapping_intervals(pairs))
