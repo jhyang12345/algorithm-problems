3b82ad62fec341472b6555c90e5b8947aba656cf
jhyang12345 <jhyang12345@naver.com>
2021-03-05 00:20:00 +0900

    Finished DFS using stacks

--- google_prep/dfs_stack.py
+++ google_prep/dfs_stack.py
@@ -1,8 +1,24 @@
 def dfs(graph, cur):
-    
+    visited = {}
+    stack = []
+    stack.append(cur)
+    while stack:
+        s = stack.pop()
+        if s not in visited:
+            print(s)
+            visited[s] = True
+        for node in graph[s]:
+            if node not in visited:
+                stack.append(node)
+
 
 graph = {
     1: [2, 3],
-    2: [4],
-    3: [5, 6]
+    2: [4, 1],
+    3: [5, 6, 1],
+    4: [2],
+    5: [3],
+    6: [3],
 }
+
+dfs(graph, 1)
